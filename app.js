adaab95c04845c403dcb14228c10eb8aea33a998
Jessica Grinberg <jessgrinberg@gmail.com>
2015-08-17 09:22:52 -0700

    can upvote existing post

--- app.js
+++ app.js
@@ -15,9 +15,13 @@ angular.module('endorphNews', [])
 		$scope.addPost = function(){
 
 			if(!$scope.title || $scope.title === '') { return; }
-			
+
   			$scope.posts.push({title: $scope.title, upvotes: 0});
   			$scope.title = '';
+		};
+
+		$scope.incrementUpvotes = function(post) {
+  			post.upvotes += 1;
 		};
 
 }]);
