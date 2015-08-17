0ee6e55912a7b83f97f4ab1ec5fa94e420bee2dd
Jessica Grinberg <jessgrinberg@gmail.com>
2015-08-17 11:00:22 -0700

    bootstrap

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
 angular.module('endorphNews', [])
-
-	angular.module('endorphNews', [])
-		.factory('posts', [function(){
-	    	var o = {
-		    	posts: []
-		  	};
-		  	return o;
-	}])
-
 	.controller('MainCtrl', [
 		'$scope',
-		'posts',
-		function($scope, posts){
-  		// $scope.test = 'Hello world!';
+		function($scope){
+  		$scope.test = 'Hello world!';
 
-  		$scope.posts = posts.posts;
-
-  // 		$scope.posts = [
-		//   {title: 'post 1', upvotes: 5},
-		//   {title: 'post 2', upvotes: 2},
-		//   {title: 'post 3', upvotes: 15},
-		//   {title: 'post 4', upvotes: 9},
-		//   {title: 'post 5', upvotes: 4}
-		// ];
+  		$scope.posts = [
+		  {title: 'post 1', upvotes: 5},
+		  {title: 'post 2', upvotes: 2},
+		  {title: 'post 3', upvotes: 15},
+		  {title: 'post 4', upvotes: 9},
+		  {title: 'post 5', upvotes: 4}
+		];
 
 		$scope.addPost = function(){
 		  if(!$scope.title || $scope.title === '') { return; }
-
+		  
 		  $scope.posts.push({
 		    title: $scope.title,
 		    link: $scope.link,
@@ -41,5 +29,6 @@ angular.module('endorphNews', [])
 		};
 
 }]);
+
 
 
