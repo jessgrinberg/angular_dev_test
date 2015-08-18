a73abc9987649004d59f618399bffef2fdae5e27
Jessica Grinberg <jessgrinberg@gmail.com>
2015-08-17 17:10:17 -0700

    angular front end

--- app.js
+++ app.js
@@ -75,7 +75,20 @@ angular.module('endorphNews', ['ui.router'])
 		'posts',
 		function($scope, $stateParams, posts){
 			$scope.post = posts.posts[$stateParams.id];
+
+$scope.addComment = function(){
+		  if($scope.body === '') { return; }
+		  $scope.post.comments.push({
+		    body: $scope.body,
+		    author: 'user',
+		    upvotes: 0
+		  });
+		  $scope.body = '';
+		};
+
+
 		}]);
+
 
 
 	
