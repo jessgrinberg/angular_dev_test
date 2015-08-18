f93f5ab0b16e9b78b7642df26b47ef7059de3612
Jessica Grinberg <jessgrinberg@gmail.com>
2015-08-18 09:28:29 -0700

    testing rest routes working

--- endorph-news/app.js
+++ endorph-news/app.js
@@ -1,3 +1,9 @@
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+mongoose.connect('mongodb://localhost/news');
+
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -8,10 +14,7 @@ var bodyParser = require('body-parser');
 
 
 
-var mongoose = require('mongoose');
-require('./models/Posts');
-require('./models/Comments');
-mongoose.connect('mongodb://localhost/news');
+
 
 
 var routes = require('./routes/index');
