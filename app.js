f8fa396484e409d02b95e819f977c751c334c14e
Jessica Grinberg <jessgrinberg@gmail.com>
2015-08-17 18:45:15 -0700

    fix comment

--- app.js
+++ app.js
@@ -35,30 +35,30 @@ angular.module('endorphNews', ['ui.router'])
 		'$scope',
 		'posts',
 		function($scope, posts){
-  		$scope.test = 'Hello world!';
+  		// $scope.test = 'Hello world!';
 
   		$scope.posts = posts.posts;
 
-  		$scope.posts = [
-		  {title: 'post 1', upvotes: 5},
-		  {title: 'post 2', upvotes: 2},
-		  {title: 'post 3', upvotes: 15},
-		  {title: 'post 4', upvotes: 9},
-		  {title: 'post 5', upvotes: 4}
-		];
+  // 		$scope.posts = [
+		//   {title: 'post 1', upvotes: 5},
+		//   {title: 'post 2', upvotes: 2},
+		//   {title: 'post 3', upvotes: 15},
+		//   {title: 'post 4', upvotes: 9},
+		//   {title: 'post 5', upvotes: 4}
+		// ];
 
 		$scope.addPost = function(){
 		  if(!$scope.title || $scope.title === '') { return; }
 		  
-		  $scope.posts.push({
-		    title: $scope.title,
-		    link: $scope.link,
-		    upvotes: 0,
-			comments: [
-			    {author: 'Joe', body: 'Cool post!', upvotes: 0},
-			    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-			]
-		  });
+		$scope.posts.push({
+		  title: $scope.title,
+		  link: $scope.link,
+		  upvotes: 0,
+		  comments: [
+		    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+		    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+		  ]
+		});
 		  $scope.title = '';
 		  $scope.link = '';
 		};
@@ -76,14 +76,14 @@ angular.module('endorphNews', ['ui.router'])
 		function($scope, $stateParams, posts){
 			$scope.post = posts.posts[$stateParams.id];
 
-$scope.addComment = function(){
-		  if($scope.body === '') { return; }
-		  $scope.post.comments.push({
-		    body: $scope.body,
-		    author: 'user',
-		    upvotes: 0
-		  });
-		  $scope.body = '';
+		$scope.addComment = function(){
+				  if($scope.body === '') { return; }
+				  $scope.post.comments.push({
+				    body: $scope.body,
+				    author: 'user',
+				    upvotes: 0
+				  });
+				  $scope.body = '';
 		};
 
 
